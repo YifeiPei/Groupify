6104a3db0d10f95bb392ac591529269459cdaa00
IvanEgg <ivancx2008@hotmail.com>
2014-01-21 14:18:19 +1030

    Ivan: GROUP-18: try to fix bootstrap issue again 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,5 @@ Trunk::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+config.assets.precompile = [/^[-_a-zA-Z0-9]*\..*/]
 end
