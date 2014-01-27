dac9cae107e992219c33e2fd4eb8d58f2881a1e6
IvanEgg <ivancx2008@hotmail.com>
2014-01-27 14:54:48 +1030

    Ivan: GROUP-39: change mode for all sh files

--- setup_linux.sh
+++ setup_linux.sh
@@ -3,4 +3,5 @@ sudo bundle install
 sudo mysql.server start
 rake db:create
 rake db:migrate
+chmod 755 *.sh
 echo "Done! Now you can run ./start_*.sh"
--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -3,4 +3,5 @@ sudo bundle install
 sudo service mysql start
 rake db:create
 rake db:migrate
+chmod 755 *.sh
 echo "Done! Now you can run ./start_*.sh"
