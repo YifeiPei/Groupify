f84ce7e33b55a3d45622d73d93b8ce093a45b83e
Des Wagner <deswagnermedia@gmail.com>
2014-01-31 10:14:35 +1030

    GROUP-1: Slightly improved class show page to display title, and currently outputs course.inspect. Making tiny commits as I encountered a problem in this area.

--- app/controllers/class_controller.rb
+++ app/controllers/class_controller.rb
@@ -24,4 +24,26 @@ class ClassController < ApplicationController
        flash[:color] = "invalid"
     end
   end
+
+  def show
+     @current_course = Course.find_by(user_id: session[:user_id], name: params[:id])
+    flash[:notice] = @current_course.inspect
+
+    # Load csv file and create list of students
+    # Retrieve file from db, this depends on the upload file implementation
+    # and will be written once that has been merged in.
+
+    if @current_course.filelocation != nil
+      # Parse file into list of students
+     # @student_list = []
+     # CSV.foreach(@current_course.filelocation) do |row|
+     #   student = Student.new (row[0], row[1], row[2])
+     #   @student_list << student
+     # end
+    #else
+    #  @student_list = nil
+    end
+  #end
+  end
+
 end
