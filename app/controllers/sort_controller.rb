b46a81ca5d07a1bbe6dee8018d69c109005b9b14
Des Wagner <deswagnermedia@gmail.com>
2014-01-31 16:03:47 +1030

    GROUP-1: Added sort button, links to sort index page.

--- app/controllers/sort_controller.rb
+++ app/controllers/sort_controller.rb
@@ -1,4 +1,10 @@
 class SortController < ApplicationController
   def index
+    @current_course = Course.find_by(user_id: session[:user_id], name: params[:name])
+    @sort = {}
+  end
+
+  def sort
+    
   end
 end
