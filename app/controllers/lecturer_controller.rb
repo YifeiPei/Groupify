a26e75205ce90399ae7502cf3d73e1eeaee951ac
IvanEgg <ivancx2008@hotmail.com>
2014-02-05 12:59:04 +1030

    Ivan: GROUP-88: fix some conflicts

--- app/controllers/lecturer_controller.rb
+++ app/controllers/lecturer_controller.rb
@@ -1,3 +1,10 @@
 class LecturerController < LecturerApplicationController
-  layout 'lecturer_application'  
+   layout 'lecturer_application'
+     def index
+    @courses = Course.where(user_id: session[:user_id])
+  end
+    def logout
+		session[:user_id] = nil
+		redirect_to "/"
+	end
 end
