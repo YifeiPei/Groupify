57c7c3c5479ce8136f4664a341900d2e9b60e395
Des Wagner <deswagnermedia@gmail.com>
2014-02-10 12:54:36 +1030

    GROUP-105: Uploads location is now in rails.root/uploads

--- app/controllers/class_controller.rb
+++ app/controllers/class_controller.rb
@@ -43,19 +43,20 @@ class ClassController < LecturerApplicationController
     # Retrieve file from db, this depends on the upload file implementation
     # and will be written once that has been merged in.
 
-   # if @current_course.filelocation != nil
+    if @current_course.filelocation != nil
       # Parse file into list of students
+      filename = @current_course.filelocation
       @student_list = []
       #CSV.foreach(@current_course.filelocation) do |row|
-      CSV.foreach("#{Rails.root}/uploads/access_adelaide.csv") do |row|
+      CSV.foreach("#{Rails.root}/#{filename}") do |row|
         @student_list << row
         #flash[:notice] = "row" + row[0] + row [1] + row[2]
         #student = Student.new (row[0], row[1], row[2])
         #@student_list << student
       end
-    #else
-     # @student_list = nil
-    #end
+    else
+      @student_list = nil
+    end
     #render 'show'
   
   end
