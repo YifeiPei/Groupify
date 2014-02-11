f570c32fbc25ea366154e602930364400db18842
Des Wagner <deswagnermedia@gmail.com>
2014-02-12 10:17:52 +1030

    GROUP-86: hooked up sort function. Works with csv data.

--- app/controllers/sort_controller.rb
+++ app/controllers/sort_controller.rb
@@ -1,3 +1,5 @@
+require 'Grouper'
+
 class SortController < ApplicationController
   layout 'lecturer_application'
   
@@ -7,7 +9,36 @@ class SortController < ApplicationController
   end
 
   def sort
+    #render :text => 'in sort function'
+    @existing_config = SortConfig.find_by(course_id: session[:course_id])
+  	if @existing_config.blank?
+      redirect_to '/sort/index'
+    end
+    if @existing_config.algorithm == 0
+      #flash[:notice] = 'Random sort'
+      sorter = Grouper::Random_sorter.new
+    elsif @existing_config.algorithm == 1
+      # Do evolutionary sort
+    end
     
+    # Get student list from csv file
+    course = Course.find_by(id: session[:course_id])
+    filename = course.filelocation
+    source = "#{Rails.root}/uploads/" + filename 
+    student_list = sorter.get_student_list_from_csv( source )
+
+    # Set parameters
+    group_size = @existing_config.group_size
+
+    #render :text => group_size
+    # Sort student list
+    sorted_list = sorter.sort( student_list, group_size )
+
+    # Store sorted list
+    destination = "#{Rails.root}/uploads/" + filename + '-sorted.csv'
+    sorter.write_group_numbers_to_csv( sorted_list, destination )
+
+    redirect_to '/class/sorted'
   end
   
   def save_config  
@@ -32,6 +63,8 @@ class SortController < ApplicationController
     else
    	 SortConfig.where(:course_id => session[:course_id]).update_all(course_id: session[:course_id], algorithm: params[:algorithm], age: params[:age], gpa: params[:gpa], degree: params[:degree])
     end
-    redirect_to '/class/sorted'
+    # Run sort algorithm
+    sort
+    #redirect_to '/class/sorted'
   end
 end
