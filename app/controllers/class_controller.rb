aeb39a0de78a1f8e25782d12da6e52eea10915bc
IvanEgg <ivancx2008@hotmail.com>
2014-02-20 16:39:45 +1030

    Ivan: GROUP-142: validation add when add class

--- app/controllers/class_controller.rb
+++ app/controllers/class_controller.rb
@@ -16,6 +16,9 @@ class ClassController < LecturerApplicationController
   end
 
   def create
+  	if params[:file].blank? || params[:group_size].to_i <= 0 || File.extname(params[:file].original_filename) != ".csv"
+  		redirect_to "/lecturer"
+	else
     @course = Course.new do |c|
       c.name = params[:course][:name]
       c.user_id = session[:user_id]
@@ -24,17 +27,17 @@ class ClassController < LecturerApplicationController
     #  flash[:notice] = "Course added."
       flash[:color] = "valid"
       	session[:course_id] = @course.id
-
-  		Student.import(params[:file], @course.id)
+      	if Student.import(params[:file], @course.id)
     	@sort_config = SortConfig.new do |sc|
     	sc.course_id = session[:course_id]
     	sc.group_size = params[:group_size]
     	end
     	@sort_config.save
 		redirect_to "/sort/sort"
+		end
     else
-       flash[:notice] = "Course not added."
-       flash[:color] = "invalid"
+		redirect_to "/lecturer"
+    end
     end
   end
 
