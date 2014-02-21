f3b9b04db916cc09c59e7e77344419fb5a0ed182
Des Wagner <deswagnermedia@gmail.com>
2014-02-21 22:16:58 +1030

    GROUP-144: Added date capture for first use and last login.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -9,6 +9,9 @@ class LoginController < ApplicationController
     if authorized_user
       session[:user_id] = authorized_user.id
       #flash[:notice] = "Wow Welcome again, you logged in as #{authorized_user.username}"
+      user = User.find_by(id: session[:user_id])
+      user.last_login = Date.current
+      user.save
       redirect_to "/lecturer"
     else
       flash[:notice] = "Invalid Username or Password"
