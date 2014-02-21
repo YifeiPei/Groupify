4a31aaaad0f0066854cca6b3c0223fdd3b2adc4a
IvanEgg <ivancx2008@hotmail.com>
2014-02-21 12:59:42 +1030

    Ivan: GROUP-142: Add error msg

--- app/controllers/class_controller.rb
+++ app/controllers/class_controller.rb
@@ -16,31 +16,42 @@ class ClassController < LecturerApplicationController
   end
 
   def create
-  	if params[:file].blank? || params[:group_size].to_i <= 0 || File.extname(params[:file].original_filename) != ".csv"
+   	if params[:course][:name].blank?
+   		session[:err_msg1] = "Course name cannot be blank."
+	end
+  	if params[:file].blank? 
+   		session[:err_msg2] = "You need to import a file."
+  	elsif File.extname(params[:file].original_filename) != ".csv"
+   		session[:err_msg2] = "You need to import a csv file."
+	end
+	
+  	if params[:group_size].to_i <= 0 
+   		session[:err_msg4] = "Group size cannot less than 0."
+	end
+
+  	if params[:course][:name].blank? || params[:file].blank? || params[:group_size].to_i <= 0 || File.extname(params[:file].original_filename) != ".csv"
   		redirect_to "/lecturer"
 	else
-    @course = Course.new do |c|
-      c.name = params[:course][:name]
-      c.user_id = session[:user_id]
-    end
-    if @course.save
-    #  flash[:notice] = "Course added."
-      flash[:color] = "valid"
+   	 	@course = Course.new do |c|
+    		c.name = params[:course][:name]
+      		c.user_id = session[:user_id]
+    	end
+    	if @course.save
+    	#  flash[:notice] = "Course added."
+      	flash[:color] = "valid"
       	session[:course_id] = @course.id
-      	if Student.import(params[:file], @course.id)
+      	Student.import(params[:file], @course.id)
     	@sort_config = SortConfig.new do |sc|
-    	sc.course_id = session[:course_id]
-    	sc.group_size = params[:group_size]
+    		sc.course_id = session[:course_id]
+    		sc.group_size = params[:group_size]
     	end
     	@sort_config.save
 		redirect_to "/sort/sort"
-		end
-    else
-		redirect_to "/lecturer"
-    end
-    end
+	    else
+			redirect_to "/lecturer"
+   		end
+   	end
   end
-
   def show
     if params[:id]
       @current_course = Course.find_by(user_id: session[:user_id], id: params[:id])
