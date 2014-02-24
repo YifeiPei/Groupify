43162f5cd8aced86d89d4fd1213f07e996219842
IvanEgg <ivancx2008@hotmail.com>
2014-02-24 14:43:31 +1030

    Ivan: GROUP-146: add student back end function done

--- app/controllers/grouped_controller.rb
+++ app/controllers/grouped_controller.rb
@@ -24,4 +24,46 @@ class GroupedController < LecturerApplicationController
 	end
 	send_data csv_string, :type => "text/csv; charset=iso-8859-1; header=present", :disposition => "attachment; filename=#{@current_course.name}.csv"
   end
+  
+    def add_student
+     @current_course = Course.find_by(user_id: session[:user_id], id: session[:course_id])
+   	if params[:student][:student_id].blank?
+   		session[:err_msg][1] = "Student id cannot be blank."
+	end
+   	if params[:student][:first_name].blank?
+   		session[:err_msg][2] = "Student first name cannot be blank."
+	end
+	
+   	if params[:student][:last_name].blank?
+   		session[:err_msg][3] = "Student last name cannot be blank."
+	end
+
+	if session[:err_msg][1].blank? && session[:err_msg][2].blank? && session[:err_msg][3].blank? & session[:err_msg][4].blank?
+     	student = Student.find_by(:student_id => params[:student][:student_id], :course_id => @current_course.id) || Student.new
+    	student.student_id = params[:student][:student_id]
+    	student.first_name = params[:student][:first_name]
+    	student.last_name = params[:student][:last_name]
+    	student.degree = params[:student][:degree]
+    	student.course_id = @current_course.id
+    	student.email = "#{student.student_id}@student.adelaide.edu.au"
+    	scg = Scg.new
+    	scg.student_id = params[:student][:id]
+    	scg.course_id = @current_course.id
+    	if !params[:group_number].blank?
+    		group = Group.find_by(:course_id => @current_course.id, :number => params[:group_number]) || Group.new
+   		 	group.number = params[:group_number]
+        	group.course_id = @current_course.id
+    		group.save!
+    		scg.group_id = group.id
+    		student.group_id = group.id
+    	end
+    	scg.save!
+    	student.scgs << scg
+  	 	student.save!
+  	 	redirect_to "/grouped"
+
+	else
+   		redirect_to "/grouped"
+   	end
+  end
 end
