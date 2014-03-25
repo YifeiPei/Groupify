8835f1064a30b7c5864022fd1ba4250b89737f7d
Retief <retief@groupify.com.au>
2014-03-25 12:56:04 +1030

    GROUP-142 added file upload test

--- test/controllers/class_controller_test.rb
+++ test/controllers/class_controller_test.rb
@@ -15,12 +15,26 @@ class ClassControllerTest < ActionController::TestCase
   	assert_redirected_to(controller: "login", action: "login")
   end
 
-  test "create a class" do
+  test "create a class error" do
   	#login
   	session[:user_id] = 1 
 
-  	#params[:course][:name] = "Test Course"
-  	#get(:create, {"course" => 'name["test"]'})
- 	#assert_redirected_to(controller: "lecturer")
+  	get(:create, {"course" => {"name" => "meh"}}, {"err_msg" => Array.new})
+
+ 	  assert_redirected_to(controller: "lecturer")
+    assert_not_equal(session[:err_msg][1], "Course name cannot be blank.")
+    assert_equal(session[:err_msg][2], "You need to import a file.")
+    assert_equal(session[:err_msg][4], "Group size cannot less than 0.")
+  end
+
+  test "create a class success" do
+    #login
+    session[:user_id] = 1 
+
+    file = fixture_file_upload("access_adelaide.csv", "text/csv")
+
+    get(:create, {"course" => {"name" => "Test Course"}, "filename" => "test.csv", "file"=>file, "group_size" => 2}, {"err_msg" => Array.new})
+
+    assert_redirected_to(controller: "sort", action: "advanced_sort")
   end
 end
